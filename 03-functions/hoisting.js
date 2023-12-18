3e5d0ea32dbf6f05cee81a3e1b9b6925b78dbd35
= <=>
2023-12-18 23:08:35 +0530

    section 3 done

--- 03-functions/hoisting.js
+++ 03-functions/hoisting.js
@@ -1,6 +1,13 @@
-
+// eg 1
 func("Govind")
 
 function func(name) {
     console.log(name)
 }
+
+// eg 2
+
+console.log(test)
+// var test = 10
+const test = 10
+// let test
--- 03-functions/setTimeOut.js
+++ 03-functions/setTimeOut.js
@@ -1,3 +1,15 @@
-setTimeout(() => { console.log("Hi") }, 2000)
+// setTimeout(() => { console.log("Hi") }, 2000)
 
-setInterval(() => { console.log("set interval") }, 2000)
+// setInterval(() => { console.log("set interval") }, 2000)
+
+// eg 1
+// function greet() {
+//     console.log("Welcome")
+// }
+// setTimeout(greet, 3000)
+
+// with arg
+function greeting(name) {
+    console.log("Welcome", name)
+}
+setTimeout(greeting, 1000, "Govind")
